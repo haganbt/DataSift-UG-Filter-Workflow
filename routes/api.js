1e376d3590ced8266d91960879dce0148c3bb9a2
Ben Hagan <benhagan@gmail.com>
2014-05-27 13:17:50 -0700

    Compile route.

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,30 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+var   express = require('express')
+    , router = express.Router()
+    , ds = require('../lib/datasift')
+    ;
 
 /* POST compile. */
 router.get('/compile', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({insecticons : ["Shrapnel","Bombshell", "Kickback"]}));
-    res.end();
+
+    ds.compile(csdl, function callback(err, res, body) {
+        // pass through DS response
+        res.writeHead(res.statusCode, { 'Content-Type': 'application/json' });
+        res.write(body);
+        res.end();
+    });
+
 });
 
+
+/*
+ router.get('/compile', function(req, res) {
+ res.writeHead(200, { 'Content-Type': 'application/json' });
+ res.write(JSON.stringify({insecticons : ["Shrapnel","Bombshell", "Kickback"]}));
+ res.end();
+ });
+
+ */
 
 
 module.exports = router;
