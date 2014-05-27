807605ffabe7648fe870fcbf287ed9fbe3072b14
Ben Hagan <benhagan@gmail.com>
2014-05-27 16:13:22 -0700

    /Compile resource and associated test.

--- test/unit/api-test.js
+++ test/unit/api-test.js
@@ -1,9 +1,9 @@
 'use strict';
 process.env.NODE_ENV = 'test';
 
-
 var request = require('superagent');
-var expect = require('expect');
+var expect = require("chai").expect;
+var should = require('should');
 var express = require('express'); // 'express' is used in bodyParser if needed
 var server;
 
@@ -11,20 +11,25 @@ var server;
 describe('#REST API', function() {
 
     var app = require('../../app');
+
     before(function(){
         server = app.listen(3000);
     });
 
-    it('- POST is testing', function(done) {
-        request.post('/api/compile').send().end(function(e, res) {
-            //console.log(res.text)
-            //expect(e).to.eql(null);
-            expect(200, done);
-            done();
-        });
+    it('- /API/COMPILE resource', function(done) {
+        request.post('localhost:3000/api/compile')
+            .send({ foo: 'bar', baz: 'blaaah' })
+            .end(function(err, res){
+                expect(res).to.exist;
+                should.not.exist(err);
+                res.should.have.status(200);
+                res.text.should.include('dpu');
+                done();
+            });
     });
 
     after(function() {
         server.close();
     });
+
 });
