e0f9d2fc325b73d660700fb2827cc5fa0fa783de
Ben Hagan <benhagan@gmail.com>
2014-05-29 19:32:35 -0700

    Compile API route.

--- routes/api.js
+++ routes/api.js
@@ -7,13 +7,32 @@ var   express = require('express') // 'express' is used in bodyParser if needed
 /*
  * POST compile - wrapper to DataSift compile
  *
- * @params todo
  */
 router.post('/compile', function(req, res) {
     var auth = req.header('Authorization') || '';
     var csdl = req.rawBody || '';
 
     ds.compile(csdl, auth, function callback(err, response, body) {
+        if(err){
+            res.status(err.status || 500);
+            res.render('500', { error: err });
+        }
+        // Pass through the statusCode and Content-Type from the DS response object.
+        res.writeHead(response.statusCode, { 'Content-Type': response.headers['content-type'] });
+        res.write(body);
+        res.end();
+    });
+});
+
+/*
+ * POST create - historic preview
+ *
+ */
+router.post('/create', function(req, res) {
+    var auth = req.header('Authorization') || '';
+    var params = req.rawBody || '';
+
+    ds.create(params, auth, function callback(err, response, body) {
         if(err){
             res.status(err.status || 500);
             res.render('500', { error: err });
