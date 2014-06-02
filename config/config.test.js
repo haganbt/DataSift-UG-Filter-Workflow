a3346db256162438156801b4d8b7c18643eaabe4
Ben Hagan <benhagan@gmail.com>
2014-06-01 20:01:16 -0700

    Auto load env config files.

---
+++ config/config.test.js
@@ -0,0 +1,12 @@
+'use strict';
+var config = {};
+
+config.env = 'test';
+config.hostname = 'localhost';
+
+//DataSift - used in tests
+config.datasift = {};
+config.datasift.ds_username = "benhagan";
+config.datasift.ds_api_key = "f8d66e12d1d91a1f960702d101207fdb";
+
+module.exports = config;
