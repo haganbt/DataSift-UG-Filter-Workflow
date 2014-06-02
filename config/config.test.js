74373b1710fe78abfa9542bee2e07074bcf69dd3
Ben Hagan <benhagan@gmail.com>
2014-06-01 20:50:50 -0700

    Defaults.

--- config/config.test.js
+++ config/config.test.js
@@ -1,12 +1,11 @@
 'use strict';
 var config = {};
 
-config.env = 'test';
-config.hostname = 'localhost';
+//config.env = 'test';
 
 //DataSift - used in tests
 config.datasift = {};
-config.datasift.ds_username = "benhagan";
-config.datasift.ds_api_key = "f8d66e12d1d91a1f960702d101207fdb";
+config.datasift.ds_username = "<DS_USER>";
+config.datasift.ds_api_key = "<DS_Key>";
 
 module.exports = config;
